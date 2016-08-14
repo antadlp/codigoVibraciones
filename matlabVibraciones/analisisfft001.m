7cd247513c138d3e5447788c5ae9dfc78bafac63
antadlp <ant.adlp1986@gmail.com>
2016-08-13 20:13:13 -0500

    algo

--- matlabVibraciones/analisisfft001.m
+++ matlabVibraciones/analisisfft001.m
@@ -9,25 +9,25 @@ surface(X1, Y1, Z1,'EdgeColor', 'none'), view(3)
 Zc1 = Z1(floor(M/3), :);
 Xc1 = X1(floor(M/3), :);
 
-Sc1 = strcat('Zc1 ', S);
+Sc1 = strcat('Zc1-', S);
 figure('Name', Sc1)
 plot(Xc1, Zc1)
 
 Zc2 = Z1(floor(2*M/3), :);
 Xc2 = X1(floor(2*M/3), :);
-Sc2 = strcat('Zc2 ', S);
+Sc2 = strcat('Zc2-', S);
 figure('Name', Sc2)
 plot(Xc2, Zc2)
 
 Zc3 = Z1(:, floor(M/3));
 Yc3 = Y1(:, floor(M/3));
-Sc3 = strcat('Zc3 ', S);
+Sc3 = strcat('Zc3-', S);
 figure('Name', Sc3)
 plot(Yc3, Zc3)
 
 Zc4 = Z1(:, floor(2*M/3));
 Yc4 = Y1(:, floor(2*M/3));
-Sc4 = strcat('Zc4 ', S);
+Sc4 = strcat('Zc4-', S);
 figure('Name', Sc4)
 plot(Yc4, Zc4)
 
@@ -38,14 +38,24 @@ x2 = (-M/2:M/2-1)*(fs/M);
 y2 = x2;
 [X2 Y2] = meshgrid(x2, y2);
 
-S1P = strcat('Z1P ', S);
+S1P = strcat('Z1P-', S);
 figure('Name', S1P)
-maxP = maxValueMatrix(Z1P);
+maxP = maxValueMatrix(Z1P)
 k = 0.05;
-Z1P = Z1P/(k*maxP);
-surface(X2, Y2, Z1P,'EdgeColor', 'none'), view(3)
+Z1Pk = Z1P/(k*maxP);
+surface(X2, Y2, Z1Pk,'EdgeColor', 'none'), view(3)
 
-y = Z1P;
+l = 1;
+for i=1:M
+   for j=1:N
+      if (Z1P(i,j) >= 0.5*maxP)
+         y(l) = Z1P(i,j);
+         l = l + 1;
+      end
+   end
+end
+
+
 
 
 
